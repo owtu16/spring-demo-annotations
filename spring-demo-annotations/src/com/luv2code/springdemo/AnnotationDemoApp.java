32819b4f1b1177f321cb637a6d152d3569b39867
Melvin Cancino <melvin.cancino88@gmail.com>
2020-05-27 10:27:00 -0700

    added the configurations to get the bean from the spring container in the main method

--- spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -1,9 +1,19 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AnnotationDemoApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// read spring config file
+		ClassPathXmlApplicationContext ctx = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		// get the bean from spring container
+		Coach theCoach = ctx.getBean("thatSillyCoach",Coach.class);
+		// call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		// close the context
+		ctx.close();
 
 	}
 
