0f92da85284f37b3de72a4ffd3c50245b6315ac3
Melvin Cancino <melvin.cancino88@gmail.com>
2020-05-27 15:50:51 -0700

    Added a new Coach implementation, added sport.properties file, added line #14 on applicationContext file and injected the property values for SwimCoach on lines 9 and 11 respectively

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
@@ -0,0 +1,30 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+
+public class SwimCoach implements Coach {
+	
+	@Value("${foo.email}")
+	private String email;
+	@Value("${foo.team}")
+	private String team;
+	
+	@Autowired
+	@Qualifier("randomFortuneService")
+	private FortuneService fortuneService;
+
+	@Override
+	public String getDailyWorkout() {
+		// TODO Auto-generated method stub
+		return "Swim everyday for at least an hour";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
+	}
+
+}
