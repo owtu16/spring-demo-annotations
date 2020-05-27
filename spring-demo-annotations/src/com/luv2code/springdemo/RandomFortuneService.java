cbf867dd30027a7fbeede2d328ce01182e0f89de
Melvin Cancino <melvin.cancino88@gmail.com>
2020-05-27 13:51:42 -0700

    Added a private String array of data

--- spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -5,6 +5,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class RandomFortuneService implements FortuneService {
 
+	// create an array of string
+	private String[] data = {
+			"Beware of the wolf in sheep's clothing",
+			"Early bird gets the worm",
+			"Never stop learning"
+	};
+	
 	@Override
 	public String getFortune() {
 		// TODO Auto-generated method stub
