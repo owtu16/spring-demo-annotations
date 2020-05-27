22a05a3d63b2bdaf214c2a6c909feb57a0403723
Melvin Cancino <melvin.cancino88@gmail.com>
2020-05-27 14:45:29 -0700

    Changed the name of the Qualifier annotation to randomFortuneService

--- spring-demo-annotations/src/com/luv2code/springdemo/SoccerCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SoccerCoach.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class SoccerCoach implements Coach {
 	
 	@Autowired
-	@Qualifier("randomService")
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
 	/*
