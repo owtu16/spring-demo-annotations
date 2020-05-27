5fd31e7328546ce04185a2bf448b07447f60d924
Melvin Cancino <melvin.cancino88@gmail.com>
2020-05-27 15:06:56 -0700

    I added a print function that automates the application context configuration

--- spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -3,27 +3,42 @@ package com.luv2code.springdemo;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AnnotationDemoApp {
+	
+	public static void print(String beanText) {
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		Coach x = context.getBean(beanText,Coach.class);
+		
+		System.out.println(x.getClass().getSimpleName() + ": " + x.getDailyWorkout());
+		
+		System.out.println("Your daily fortune according to your " + 
+		x.getClass().getSimpleName() + ": " + x.getDailyFortune() + "\n");
+		
+		context.close();
+	}
 
 	public static void main(String[] args) {
 		// read spring config file
-		ClassPathXmlApplicationContext ctx = 
-				new ClassPathXmlApplicationContext("applicationContext.xml");
+//		ClassPathXmlApplicationContext ctx = 
+//				new ClassPathXmlApplicationContext("applicationContext.xml");
 		// get the bean from spring container
-		Coach theCoach = ctx.getBean("tennisCoach",Coach.class);
-		Coach soccerCoach = ctx.getBean("soccerCoach",Coach.class);
+//		Coach theCoach = ctx.getBean("tennisCoach",Coach.class);
+//		Coach soccerCoach = ctx.getBean("soccerCoach",Coach.class);
 		// call a method on the bean
-		System.out.println(theCoach.getClass().getSimpleName() + ": " + theCoach.getDailyWorkout());
-		System.out.println(soccerCoach.getClass().getSimpleName() + ": " + soccerCoach.getDailyWorkout() + "\n");
+//		System.out.println(theCoach.getClass().getSimpleName() + ": " + theCoach.getDailyWorkout());
+//		System.out.println(soccerCoach.getClass().getSimpleName() + ": " + soccerCoach.getDailyWorkout() + "\n");
 		
 		// call method to get the daily fortune
 		
-		System.out.println("Your daily fortune according to your " + 
-		theCoach.getClass().getSimpleName() + ": " + theCoach.getDailyFortune());
-		
-		System.out.println("Your daily fortune according to your " + 
-		soccerCoach.getClass().getSimpleName() + ": " + soccerCoach.getDailyFortune());
+//		System.out.println("Your daily fortune according to your " + 
+//		theCoach.getClass().getSimpleName() + ": " + theCoach.getDailyFortune());
+//		
+//		System.out.println("Your daily fortune according to your " + 
+//		soccerCoach.getClass().getSimpleName() + ": " + soccerCoach.getDailyFortune());
+		print("tennisCoach");
+		print("soccerCoach");
 		// close the context
-		ctx.close();
+//		ctx.close();
 
 	}
 
