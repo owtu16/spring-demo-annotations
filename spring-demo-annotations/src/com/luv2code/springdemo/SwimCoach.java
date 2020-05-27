3ecf771311a2b6a6e6ea97be063df91bfdba88fb
Melvin Cancino <melvin.cancino88@gmail.com>
2020-05-27 15:52:07 -0700

    Added component tag on line 8

--- spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
@@ -3,7 +3,9 @@ package com.luv2code.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SwimCoach implements Coach {
 	
 	@Value("${foo.email}")
