701a7ad0f89e848bb9d4c1a52d8d349e1ede1c64
Melvin Cancino <melvin.cancino88@gmail.com>
2020-05-28 08:27:42 -0700

    added a static print method

--- spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationDemoApp.java
@@ -3,20 +3,23 @@ package com.luv2code.springdemo;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AnnotationDemoApp {
-	
-	public static void print(String beanText) {
-		ClassPathXmlApplicationContext context = 
-				new ClassPathXmlApplicationContext("applicationContext.xml");
-		Coach x = context.getBean(beanText,Coach.class);
-		
+
+	static ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+	public static void print(String beanText, boolean includesMethods) {
+
+		Coach x = context.getBean(beanText, Coach.class);
+
 		System.out.println(x.getClass().getSimpleName() + ": " + x.getDailyWorkout());
-		
-		System.out.println("Your daily fortune according to your " + 
-		x.getClass().getSimpleName() + ": " + x.getDailyFortune() + "\n");
-		
-		
-	
-		context.close();
+
+		System.out.println("Your daily fortune according to your " + x.getClass().getSimpleName() + ": "
+				+ x.getDailyFortune() + "\n");
+		if(includesMethods) {
+			System.out.println(((SwimCoach)x).getEmail());
+			System.out.println(((SwimCoach)x).getTeam());
+		}
+//		System.out.println(beanText);
+//		context.close();
 	}
 
 	public static void main(String[] args) {
@@ -29,20 +32,20 @@ public class AnnotationDemoApp {
 		// call a method on the bean
 //		System.out.println(theCoach.getClass().getSimpleName() + ": " + theCoach.getDailyWorkout());
 //		System.out.println(soccerCoach.getClass().getSimpleName() + ": " + soccerCoach.getDailyWorkout() + "\n");
-		
+
 		// call method to get the daily fortune
-		
+
 //		System.out.println("Your daily fortune according to your " + 
 //		theCoach.getClass().getSimpleName() + ": " + theCoach.getDailyFortune());
 //		
 //		System.out.println("Your daily fortune according to your " + 
 //		soccerCoach.getClass().getSimpleName() + ": " + soccerCoach.getDailyFortune());
 		
-		print("tennisCoach");
-		print("soccerCoach");
-		print("swimCoach");
+		print("tennisCoach",false);
+		print("soccerCoach",false);
+		print("swimCoach",true);
 		// close the context
-//		ctx.close();
+		context.close();
 
 	}
 
