3682334d0f53dd382ff4b143bee8d06a8bf2c24d
Melvin Cancino <melvin.cancino88@gmail.com>
2020-05-28 10:29:50 -0700

    Added AnnotationBeanScopeDemoApp to test Scope. Scope instantiate single beans, if using Singleton, or multiple beans, if using prototype

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,32 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		// load spring config file
+		ClassPathXmlApplicationContext ctx =
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// retrieve bean from spring container
+		Coach theCoach = ctx.getBean("tennisCoach",Coach.class);
+		
+		Coach alphacoach = ctx.getBean("tennisCoach",Coach.class);
+		
+		// check if they are the same
+		
+		boolean result = (theCoach == alphacoach);
+		
+		// print out the results
+		System.out.println("\nPointing to the same object: " + result);
+		
+		System.out.println("\nMemory location for the theCoach: " + theCoach);
+		
+		System.out.println("\nMemory location for alphaCoach: " + alphacoach + "\n");
+		
+		ctx.close();
+
+	}
+
+}
