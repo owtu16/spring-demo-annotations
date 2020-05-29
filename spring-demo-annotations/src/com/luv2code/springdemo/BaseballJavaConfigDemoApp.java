a56874f50d7c08b5f7d4af641662f103df670559
Melvin Cancino <melvin.cancino88@gmail.com>
2020-05-28 23:27:08 -0700

    BaseballJavaConfigDemoApp and SportConfig class created

---
+++ spring-demo-annotations/src/com/luv2code/springdemo/BaseballJavaConfigDemoApp.java
@@ -0,0 +1,38 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class BaseballJavaConfigDemoApp {
+
+	static AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+	public static void print(String beanText, boolean includesMethods) {
+
+		Coach x = context.getBean(beanText, Coach.class);
+
+		System.out.println(x.getClass().getSimpleName() + ": " + x.getDailyWorkout());
+
+		System.out.println("Your daily fortune according to your " + x.getClass().getSimpleName() + ": "
+				+ x.getDailyFortune() + "\n");
+		if(includesMethods) {
+			System.out.println(((SwimCoach)x).getEmail());
+			System.out.println(((SwimCoach)x).getTeam() + "\n");
+		}
+//		System.out.println(beanText);
+//		context.close();
+	}
+
+	public static void main(String[] args) {
+	
+		
+//		print("tennisCoach",false);
+//		print("soccerCoach",false);
+//		print("swimCoach",true);
+//		print("hockeyCoach",false);
+		print("baseballCoach",false);
+		// close the context
+		context.close();
+
+	}
+
+}
--- spring-demo-annotations/src/com/luv2code/springdemo/SportConfig.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/SportConfig.java
@@ -1,10 +1,23 @@
 package com.luv2code.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.luv2code.springdemo")
 public class SportConfig {
+	
+	// define bean for our sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	// define bean for our baseball coach AND inject dependency
+	@Bean
+	public Coach baseballCoach() {
+		return new BaseballCoach(sadFortuneService());
+	}
 
 }
